b355cf86967bb6b3f9e857736bcf58a972e61706
KLishi <67844338+KLishi@users.noreply.github.com>
2021-04-16 16:16:22 -0700

    Complete Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { useState, useEffect} from 'react'
+ import { useState, useEffect} from 'react'
+ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
 
 const App=() => {
   const[showAddTask,setShowAddTask] = useState(false)
@@ -19,33 +22,65 @@ const App=() => {
     const data = await res.json()
     return(data)
   }
-//Add task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) + 1
-  const newTask = {id, ...task}
-  setTasks([...tasks,newTask])
   
+  // fetch task 
+  const fetchTask = async(id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return(data)
+  }
+
+//Add task
+const addTask = async(task) => {
+  // const id = Math.floor(Math.random() * 10000) + 1
+  // const newTask = {id, ...task}
+  // setTasks([...tasks,newTask])
+  const res = await fetch('http://localhost:5000/tasks',{method: 'POST',
+         headers: {'content-type': 'application/json'},body: JSON.stringify(task)
+})
+const data = await res.json()
+setTasks([...tasks,data])
 
 }
 
 //Delete task
-const deleteTask = (id) => {
+const deleteTask = async(id) => {
+  await fetch(`http://localhost:5000/tasks/${id}`,{
+    method: 'DELETE'
+  })
   setTasks(tasks.filter((task) => task.id !== id))
  
 }
 
 //Toggle Reminder
- const toggleReminder = (id) => {
-   setTasks(tasks.map((task)=> task.id === id ? {...task, reminder: !task.reminder} : task))
+ const toggleReminder = async (id) => {
+   const taskToToggle = await fetchTask(id)
+   const updatedTask = {...taskToToggle, reminder : !taskToToggle.reminder}
+   const res = await fetch(`http://localhost:5000/tasks/${id}`,{ method:'PUT',
+   headers:{'content-type':'application/json'},
+   body: JSON.stringify(updatedTask)
+  })
+  const data = await res.json()
+   setTasks(tasks.map((task)=> task.id === id ? {...task, reminder: data.reminder} : task))
     
   }
   
+  
   return (
+    <Router>
     <div className="container">
      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-     {showAddTask && <AddTask onAdd={addTask}/>}
+     
+     <Route path='/' exact render={ (props) => (
+       <>
+{showAddTask && <AddTask onAdd={addTask}/>}
      {tasks.length > 0 ?(<Tasks tasks= {tasks} onDelete={deleteTask} onToggle = {toggleReminder}/>) : ('No Tasks to show')}
+       </>
+     )}/>
+     <Route path='/about' component= {About}/>
+     <Footer />
     </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
+import {useLocation} from 'react-router-dom'
 
 const Header = ({title, onAdd, showAdd}) => {
-     
+     const location = useLocation()
     return (
         <header className='header'>
             <h1 className="title">{title}</h1>
-            <Button color= {showAdd ?  ' #b2c2bf ': '#c0ded9'} text = {showAdd ? 'Close' : "Add"} onClick={onAdd}/>
+            { location.pathname === '/' && (<Button color= {showAdd ?  ' #b2c2bf ': '#c0ded9'} text = {showAdd ? 'Close' : "Add"} onClick={onAdd}/>)}
         </header>
             
         
